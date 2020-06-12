1060885bf49124e399eff28d08d9de2862c3385d
liuyuezhangadam <liuyuezhangadam@gmail.com>
2020-06-12 23:12:51 +0800

    final

--- script/attack_cifar.sh
+++ script/attack_cifar.sh
@@ -3,9 +3,9 @@
 ATTACK=fgsm
 SEED=0
 
-python -m attack --attack $ATTACK --epoch 10 --env cifar10 --granule fc       --ltd none --n-hidden 1166   --lr 4.288e-4   --seed $SEED --wandb --save &
-python -m attack --attack $ATTACK --epoch 10 --env cifar10 --granule fc       --ltd ma   --n-hidden 1166   --lr 4.288e-4   --seed $SEED --wandb --save &
-python -m attack --attack $ATTACK --epoch 10 --env cifar10 --granule fc       --ltd none --n-hidden 5000   --lr 1e-4       --seed $SEED --wandb --save &
-python -m attack --attack $ATTACK --epoch 10 --env cifar10 --granule fc       --ltd ma   --n-hidden 5000   --lr 1e-4       --seed $SEED --wandb --save &
+#python -m attack --attack $ATTACK --epoch 10 --env cifar10 --granule fc       --ltd none --n-hidden 1166   --lr 4.288e-4   --seed $SEED --wandb --save &
+#python -m attack --attack $ATTACK --epoch 10 --env cifar10 --granule fc       --ltd ma   --n-hidden 1166   --lr 4.288e-4   --seed $SEED --wandb --save &
+#python -m attack --attack $ATTACK --epoch 10 --env cifar10 --granule fc       --ltd none --n-hidden 5000   --lr 1e-4       --seed $SEED --wandb --save &
+#python -m attack --attack $ATTACK --epoch 10 --env cifar10 --granule fc       --ltd ma   --n-hidden 5000   --lr 1e-4       --seed $SEED --wandb --save &
 python -m attack --attack $ATTACK --epoch 10 --env cifar10 --granule rc --k 4 --ltd none --n-hidden 200000 --lr 2.5e-6     --seed $SEED --wandb --save &
 python -m attack --attack $ATTACK --epoch 10 --env cifar10 --granule rc --k 4 --ltd ma   --n-hidden 200000 --lr 2.5e-6     --seed $SEED --wandb --save &
--- script/train_cifar.sh
+++ script/train_cifar.sh
@@ -2,9 +2,9 @@
 
 SEED=0
 
-python -m main --epoch 10 --env cifar10 --granule fc       --ltd none --n-hidden 1166   --lr 4.288e-4   --seed $SEED --wandb --save &
-python -m main --epoch 10 --env cifar10 --granule fc       --ltd ma   --n-hidden 1166   --lr 4.288e-4   --seed $SEED --wandb --save &
-python -m main --epoch 10 --env cifar10 --granule fc       --ltd none --n-hidden 5000   --lr 1e-4       --seed $SEED --wandb --save &
-python -m main --epoch 10 --env cifar10 --granule fc       --ltd ma   --n-hidden 5000   --lr 1e-4       --seed $SEED --wandb --save &
+#python -m main --epoch 10 --env cifar10 --granule fc       --ltd none --n-hidden 1166   --lr 4.288e-4   --seed $SEED --wandb --save &
+#python -m main --epoch 10 --env cifar10 --granule fc       --ltd ma   --n-hidden 1166   --lr 4.288e-4   --seed $SEED --wandb --save &
+#python -m main --epoch 10 --env cifar10 --granule fc       --ltd none --n-hidden 5000   --lr 1e-4       --seed $SEED --wandb --save &
+#python -m main --epoch 10 --env cifar10 --granule fc       --ltd ma   --n-hidden 5000   --lr 1e-4       --seed $SEED --wandb --save &
 python -m main --epoch 10 --env cifar10 --granule rc --k 4 --ltd none --n-hidden 200000 --lr 2.5e-6     --seed $SEED --wandb --save &
 python -m main --epoch 10 --env cifar10 --granule rc --k 4 --ltd ma   --n-hidden 200000 --lr 2.5e-6     --seed $SEED --wandb --save &
